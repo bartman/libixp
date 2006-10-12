0eefc880a8b7396bd8cb2cfa1cc37fd3bf824e5b
arg@suckless.org <>
2006-10-12 13:14:13 +0200

    initial commit

--- intmap.c
+++ intmap.c
@@ -1,36 +1,38 @@
 /* This file is derived from src/lib9p/intmap.c from plan9port */
 /* See LICENCE.p9p for terms of use */
-
 #include <stdlib.h>
-#include "ixp_local.h"
+#include "ixp.h"
+#define USED(v) if(v){}else{}
 
 struct Intlist {
-	ulong	id;
+	unsigned long	id;
 	void*		aux;
 	Intlist*	link;
-	uint	ref;
+	unsigned int	ref;
 };
 
-static ulong
-hashid(Intmap *map, ulong id) {
+static unsigned long
+hashid(Intmap *map, unsigned long id)
+{
 	return id%map->nhash;
 }
 
 static void
-nop(void *v) {
+nop(void *v)
+{
 	USED(v);
 }
 
 void
-initmap(Intmap *m, ulong nhash, void *hash) {
+initmap(Intmap *m, unsigned long nhash, void *hash)
+{
 	m->nhash = nhash;
 	m->hash = hash;
-
-	thread->initrwlock(&m->lk);
 }
 
 static Intlist**
-llookup(Intmap *map, ulong id) {
+llookup(Intmap *map, unsigned long id)
+{
 	Intlist **lf;
 
 	for(lf=&map->hash[hashid(map, id)]; *lf; lf=&(*lf)->link)
@@ -40,11 +42,12 @@ llookup(Intmap *map, ulong id) {
 }
 
 void
-freemap(Intmap *map, void (*destroy)(void*)) {
+freemap(Intmap *map, void (*destroy)(void*))
+{
 	int i;
 	Intlist *p, *nlink;
 
-	if(destroy == nil)
+	if(destroy == NULL)
 		destroy = nop;
 	for(i=0; i<map->nhash; i++){
 		for(p=map->hash[i]; p; p=nlink){
@@ -53,76 +56,68 @@ freemap(Intmap *map, void (*destroy)(void*)) {
 			free(p);
 		}
 	}
-
-	thread->rwdestroy(&map->lk);
 }
-
 void
-execmap(Intmap *map, void (*run)(void*)) {
+execmap(Intmap *map, void (*run)(void*))
+{
 	int i;
 	Intlist *p, *nlink;
 
-	thread->rlock(&map->lk);
 	for(i=0; i<map->nhash; i++){
 		for(p=map->hash[i]; p; p=nlink){
-			thread->runlock(&map->lk);
 			nlink = p->link;
 			run(p->aux);
-			thread->rlock(&map->lk);
 		}
 	}
-	thread->runlock(&map->lk);
 }
 
 void*
-lookupkey(Intmap *map, ulong id) {
+lookupkey(Intmap *map, unsigned long id)
+{
 	Intlist *f;
 	void *v;
 
-	thread->rlock(&map->lk);
 	if((f = *llookup(map, id)))
 		v = f->aux;
 	else
-		v = nil;
-	thread->runlock(&map->lk);
+		v = NULL;
 	return v;
 }
 
 void*
-insertkey(Intmap *map, ulong id, void *v) {
+insertkey(Intmap *map, unsigned long id, void *v)
+{
 	Intlist *f;
 	void *ov;
-	ulong h;
+	unsigned long h;
 
-	thread->wlock(&map->lk);
 	if((f = *llookup(map, id))){
 		/* no decrement for ov because we're returning it */
 		ov = f->aux;
 		f->aux = v;
 	}else{
-		f = emallocz(sizeof *f);
+		f = ixp_emallocz(sizeof(*f));
 		f->id = id;
 		f->aux = v;
 		h = hashid(map, id);
 		f->link = map->hash[h];
 		map->hash[h] = f;
-		ov = nil;
+		ov = NULL;
 	}
-	thread->wunlock(&map->lk);
 	return ov;	
 }
 
 int
-caninsertkey(Intmap *map, ulong id, void *v) {
+caninsertkey(Intmap *map, unsigned long id, void *v)
+{
 	Intlist *f;
 	int rv;
-	ulong h;
+	unsigned long h;
 
-	thread->wlock(&map->lk);
 	if(*llookup(map, id))
 		rv = 0;
 	else{
-		f = emallocz(sizeof *f);
+		f = ixp_emallocz(sizeof *f);
 		f->id = id;
 		f->aux = v;
 		h = hashid(map, id);
@@ -130,22 +125,20 @@ caninsertkey(Intmap *map, ulong id, void *v) {
 		map->hash[h] = f;
 		rv = 1;
 	}
-	thread->wunlock(&map->lk);
 	return rv;	
 }
 
 void*
-deletekey(Intmap *map, ulong id) {
+deletekey(Intmap *map, unsigned long id)
+{
 	Intlist **lf, *f;
 	void *ov;
 
-	thread->wlock(&map->lk);
 	if((f = *(lf = llookup(map, id)))){
 		ov = f->aux;
 		*lf = f->link;
 		free(f);
 	}else
-		ov = nil;
-	thread->wunlock(&map->lk);
+		ov = NULL;
 	return ov;
 }
